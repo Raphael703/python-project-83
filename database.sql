e9c8035da5fcc5102511985849080022bf549ba3
Raphael <raphael.mukha@gmail.com>
2023-12-20 17:03:31 +0300

    Expand database.sql by url_checks table

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE urls (
 	name VARCHAR(255) NOT NULL UNIQUE,
     created_at DATE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER REFERENCES urls(id) ON DELETE CASCADE,
+    status_code INTEGER,
+    h1 varchar(255),
+    title varchar(255),
+    description varchar(255),
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
+);
